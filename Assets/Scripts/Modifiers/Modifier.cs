5fcd6cf0f188714254e84ebff95b5fed06bb4753
Damie <damienmarble@gmail.com>
2020-05-01 11:08:04 -0400

    Readded modifier

---
+++ Assets/Scripts/Modifiers/Modifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IdleFramework
+{
+    public class Modifier : Updates
+    {
+        private readonly ModifierDefinition definition;
+        private bool isActive;
+        public bool IsActive => isActive;
+
+        public Modifier(ModifierDefinition definition)
+        {
+            this.definition = definition;
+        }
+
+        public void Update(IdleEngine engine, float deltaTime)
+        {
+            isActive = definition.IsActiveMatcher.Matches(engine);
+        }
+    }
+}

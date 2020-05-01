a32ce48ddff3a715bb8550071ce005249214503b
Damie <damienmarble@gmail.com>
2020-05-01 11:04:17 -0400

    Added matchers for availability and enabled matchers.

---
+++ Assets/Scripts/Matchers/Entities/EntityEnabledMatcher.cs
@@ -0,0 +1,14 @@
+﻿using IdleFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IdleFramework.State.Matchers
+{
+    public class EntityEnabledMatcher : EntityBooleanPropertyMatcher
+    {
+        public EntityEnabledMatcher(string entityKey) : base(entityKey, "Enabled", true)
+        {
+        }
+    }
+}

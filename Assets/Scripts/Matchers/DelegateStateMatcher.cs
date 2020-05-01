d990f6e98031b8a5c01a1d482ebf45c8f295f74a
Damie <damienmarble@gmail.com>
2020-05-01 11:03:53 -0400

    Add matcher which defer to delegate to match.

---
+++ Assets/Scripts/Matchers/DelegateStateMatcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IdleFramework
+{
+    public class DelegateStateMatcher : StateMatcher
+    {
+        private readonly Func<IdleEngine, bool> delegateFunc;
+
+        public DelegateStateMatcher(Func<IdleEngine, bool> delegateFunc)
+        {
+            this.delegateFunc = delegateFunc;
+        }
+
+        public bool Matches(IdleEngine engine)
+        {
+            return delegateFunc.Invoke(engine);
+        }
+    }
+}

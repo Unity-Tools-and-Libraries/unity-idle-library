eceed621252850f32f342493b33cf5dc514095d5
Damie <damienmarble@gmail.com>
2020-05-01 10:45:13 -0400

    Add classes for references to entity bought and quantity

---
+++ Assets/Scripts/References/Entities/EntityQuantityReference.cs
@@ -0,0 +1,23 @@
+﻿using BreakInfinity;
+using IdleFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IdleFramework
+{
+    public class EntityQuantityReference : NumberContainer
+    {
+        private readonly string entityName;
+
+        public EntityQuantityReference(string entityName)
+        {
+            this.entityName = entityName;
+        }
+
+        public BigDouble Get(IdleEngine engine)
+        {
+            return engine.GetEntity(entityName).Quantity;
+        }
+    }
+}

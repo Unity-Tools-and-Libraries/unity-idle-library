f51ede78d6d2087cec16a26edd9762d8fd2d98cd
Damien Marble <damienmarble@gmail.com>
2022-12-11 19:42:07 -0500

    Add click income base and multiplier fields

--- Assets/Runtime/Scripts/Engine/Modules/Clicker/ResourceHolder.cs
+++ Assets/Runtime/Scripts/Engine/Modules/Clicker/ResourceHolder.cs
@@ -9,7 +9,10 @@ namespace io.github.thisisnozaku.idle.framework.Engine.Modules.Clicker
     {
         public BigDouble Quantity { get; set; }
         public BigDouble TotalIncome { get; set; }
-        public BigDouble ClickIncome { get; set; } = 1;
+        public BigDouble ClickIncome => this.ClickIncomeBase * this.ClickIncomeMultiplier;
+
+        public BigDouble ClickIncomeBase { get; set; } = 1;
+        public BigDouble ClickIncomeMultiplier { get; set; } = 1;
 
         public bool Spend(BigDouble quantity)
         {

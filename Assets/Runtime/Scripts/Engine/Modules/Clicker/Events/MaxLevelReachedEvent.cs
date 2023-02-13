c192cb9aea7402f759126e69753589b8113d38c3
Damien Marble <damienmarble@gmail.com>
2023-02-12 21:06:21 -0800

    Added to namespace

--- Assets/Runtime/Scripts/Engine/Modules/Clicker/Events/MaxLevelReachedEvent.cs
+++ Assets/Runtime/Scripts/Engine/Modules/Clicker/Events/MaxLevelReachedEvent.cs
@@ -4,23 +4,25 @@ using BreakInfinity;
 using io.github.thisisnozaku.idle.framework.Engine.Modules.Clicker.Definitions;
 using io.github.thisisnozaku.scripting.context;
 using UnityEngine;
-
-public class MaxLevelReachedEvent : IScriptingContext
+namespace io.github.thisisnozaku.idle.framework.Engine.Modules.Clicker.Events
 {
-    public const string EventName = "MaxLevelReached";
-    private Dictionary<string, object> context;
-
-    public MaxLevelReachedEvent(Upgrade upgrade, BigDouble level)
+    public class MaxLevelReachedEvent : IScriptingContext
     {
-        context = new Dictionary<string, object>()
+        public const string EventName = "MaxLevelReached";
+        private Dictionary<string, object> context;
+
+        public MaxLevelReachedEvent(Upgrade upgrade, BigDouble level)
+        {
+            context = new Dictionary<string, object>()
         {
             { "level", level },
             { "upgrade", upgrade }
         };
-    }
+        }
 
-    public Dictionary<string, object> GetContextVariables()
-    {
-        return context;
+        public Dictionary<string, object> GetContextVariables()
+        {
+            return context;
+        }
     }
 }

eab8faa4dd68bf62316d56f8ec816fe2a27f531a
Damien Marble <damienmarble@gmail.com>
2023-02-15 00:55:38 -0800

    Tests for scripting metamethods

---
+++ Assets/Tests/Engine/Scripting/BigDoubleScriptingTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BreakInfinity;
+using NUnit.Framework;
+using UnityEngine;
+namespace io.github.thisisnozaku.idle.framework.Tests.Engine.Scripting
+{
+    public class BigDoubleScriptingTest : TestsRequiringEngine
+    {
+        [Test]
+        public void Concatenation()
+        {
+            Assert.AreEqual("12", engine.Scripting.EvaluateStringAsScript("return '1' .. x", Tuple.Create<string, object>("x", new BigDouble(2))).String);
+        }
+    }
+}
